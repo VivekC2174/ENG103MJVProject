b52c48136c7a57d018ffaa2b5f7209eea062f3d8
James <pitorjames@gmail.com>
2021-10-20 13:52:49 +1000

    Added code to measure BPM and SP02. RBG LED changes colour to correspond with BPM

--- src/BloodOxygenSensor.py
+++ src/BloodOxygenSensor.py
@@ -1,72 +1,115 @@
-from max30102 import MAX30102
-import hrcalc
-import threading
+from __future__ import print_function
+import qwiic_max3010x
 import time
-import numpy as np
+import sys
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+RED = 25
+GREEN = 24
+BLUE = 23
+
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
+GPIO.setup(BLUE, GPIO.OUT)
 
 
-class HeartRateMonitor(object):
-    """
-    A class that encapsulates the max30102.py device into a thread
-    """
+def millis():
+    return int(round(time.time() * 1000))
 
-    LOOP_TIME = 0.01
 
-    def __init__(self, print_raw=False, print_result=False):
-        self.bpm = 0
-        if print_raw is True:
-            print('IR, Red')
-        self.print_raw = print_raw
-        self.print_result = print_result
+def LEDs(red, green, blue):
+    GPIO.output(RED, red)
+    GPIO.output(GREEN, green)
+    GPIO.output(BLUE, blue)
 
-    def run_sensor(self):
-        sensor = MAX30102()
-        ir_data = []
-        red_data = []
-        bpms = []
 
-        # run until told to stop
-        while not self._thread.stopped:
-            # check if any data is available
-            num_bytes = sensor.get_data_present()
-            if num_bytes > 0:
-                # grab all the data and stash it into arrays
-                while num_bytes > 0:
-                    red, ir = sensor.read_fifo()
-                    num_bytes -= 1
-                    ir_data.append(ir)
-                    red_data.append(red)
-                    if self.print_raw:
-                        print("{0}, {1}".format(ir, red))
+def runExample():
+    print("\nSparkFun MAX3010x Photodetector - Example 5\n")
+    sensor = qwiic_max3010x.QwiicMax3010x()
 
-                while len(ir_data) > 100:
-                    ir_data.pop(0)
-                    red_data.pop(0)
+    if sensor.begin() == False:
+        print("The Qwiic MAX3010x device isn't connected to the system. Please check your connection", \
+              file=sys.stderr)
+        return
+    else:
+        print("The Qwiic MAX3010x is connected.")
 
-                if len(ir_data) == 100:
-                    bpm, valid_bpm, spo2, valid_spo2 = hrcalc.calc_hr_and_spo2(ir_data, red_data)
-                    if valid_bpm:
-                        bpms.append(bpm)
-                        while len(bpms) > 4:
-                            bpms.pop(0)
-                        self.bpm = np.mean(bpms)
-                        if (np.mean(ir_data) < 50000 and np.mean(red_data) < 50000):
-                            self.bpm = 0
-                            if self.print_result:
-                                print("Finger not detected")
-                        if self.print_result:
-                            print("BPM: {0}, SpO2: {1}".format(self.bpm, spo2))
+    print("Place your index finger on the sensor with steady pressure.")
 
-            time.sleep(self.LOOP_TIME)
+    if sensor.setup() == False:
+        print("Device setup failure. Please check your connection", \
+              file=sys.stderr)
+        return
+    else:
+        print("Setup complete.")
 
-        sensor.shutdown()
+    sensor.setPulseAmplitudeRed(0x0A)  # Turn Red LED to low to indicate sensor is running
+    sensor.setPulseAmplitudeGreen(0)  # Turn off Green LED
 
-    def start_sensor(self):
-        self._thread = threading.Thread(target=self.run_sensor)
-        self._thread.stopped = False
-        self._thread.start()
+    RATE_SIZE = 4  # Increase this for more averaging. 4 is good.
+    rates = list(range(RATE_SIZE))  # list of heart rates
+    rateSpot = 0
+    lastBeat = 0  # Time at which the last beat occurred
+    beatsPerMinute = 0.00
+    beatAvg = 0
+    samplesTaken = 0  # Counter for calculating the Hz or read rate
+    startTime = millis()  # Used to calculate measurement rate
 
-    def stop_sensor(self, timeout=2.0):
-        self._thread.stopped = True
-        self.bpm = 0
-        self._thread.join(timeout)
+    while True:
+        sp02value = sensor.getIR() / (sensor.getIR() + sensor.getRed())
+        irValue = sensor.getIR()
+        samplesTaken += 1
+        if sensor.checkForBeat(irValue) == True:
+            # We sensed a beat!
+            print('BEAT')
+            delta = (millis() - lastBeat)
+            lastBeat = millis()
+
+            beatsPerMinute = 60 / (delta / 1000.0)
+            beatsPerMinute = round(beatsPerMinute, 1)
+
+            if beatsPerMinute < 255 and beatsPerMinute > 20:
+                rateSpot += 1
+                rateSpot %= RATE_SIZE  # Wrap variable
+                rates[rateSpot] = beatsPerMinute  # Store this reading in the array
+
+                # Take average of readings
+                beatAvg = 0
+                for x in range(0, RATE_SIZE):
+                    beatAvg += rates[x]
+                beatAvg /= RATE_SIZE
+                beatAvg = round(beatAvg)
+
+        Hz = round(float(samplesTaken) / ((millis() - startTime) / 1000.0), 2)
+        if (samplesTaken % 200) == 0:
+            print(
+                'IR=', irValue, ' \t',
+                'BPM=', beatsPerMinute, '\t',
+                # 'DCE', getDCE() , '\t',\
+                'Avg=', beatAvg, '\t',
+                'Hz=', Hz,
+                'SP02=', sp02value
+            )
+            if 20 < beatsPerMinute < 70:
+                LEDs(0, 0, 0)  # turn off LED
+                LEDs(0, 1, 0)  # make Led Green
+
+            elif 70 <= beatsPerMinute <= 80:
+                LEDs(0, 0, 0)
+                LEDs(1, 1, 0)
+
+            elif beatsPerMinute > 80:
+                LEDs(0, 0, 0)
+                LEDs(1, 0, 0)
+            else:
+                LEDs(0, 0, 0)
+
+
+if __name__ == '__main__':
+    try:
+        runExample()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("\nEnding Example 5")
+        sys.exit(0)
